f5b2940a2091cbde06773e4e1bba230b2ef4bef9
deibikint <davidquintr97@gmail.com>
2022-05-28 20:59:14 -0600

    Eventos finalizados y cambios en la interfaz de vista admin

---
+++ ProyectoEyS/Entidades/Vistas/Tbl_Vw_Evento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Vistas {
+    public class Tbl_Vw_Evento {
+
+        private int idEvento;
+        private DateTime fechaInicio;
+        private DateTime fechaFin;
+        private string razon;
+        private string descripcion;
+        private string empleadoSel;
+
+        public Tbl_Vw_Evento() {
+
+        }
+
+        public int IdEvento { get => idEvento; set => idEvento = value; }
+        public DateTime FechaInicio { get => fechaInicio; set => fechaInicio = value; }
+        public DateTime FechaFin { get => fechaFin; set => fechaFin = value; }
+        public string Razon { get => razon; set => razon = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public string EmpleadoSel { get => empleadoSel; set => empleadoSel = value; }
+    }
+
+}
---
+++ ProyectoEyS/Negocio/Ng_tbl_evento.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using Datos;
+using Gtk;
+
+namespace Negocio {
+    public class Ng_tbl_evento {
+
+        Conexion con = new Conexion();
+        MessageDialog ms = null;
+        StringBuilder sb = new StringBuilder();
+
+        public Ng_tbl_evento() {
+        }
+
+        public bool ExisteEvento(int idEmp) {
+            IDataReader idr = null;
+            sb.Clear();
+
+            sb.Append("SELECT * FROM BDSistemaEyS.tbl_Evento ");
+            sb.Append("where fechaFin > '" + DateTime.Now.ToString("yy-MM-dd") + "' and idEmpleado = '" + idEmp + "'");
+            try {
+                con.AbrirConexion();
+                idr = con.Leer(CommandType.Text, sb.ToString());
+                int i = 0;
+                while (idr.Read()) {
+                    i++;
+                }
+                if (i > 0)
+                    return true;
+
+                return false;
+            } catch (Exception e) {
+                ms = new MessageDialog(null, DialogFlags.Modal, MessageType.Error,
+                    ButtonsType.Ok, e.Message);
+                ms.Run();
+                ms.Destroy();
+                throw;
+            } finally {
+                idr.Close();
+                con.CerrarConexion();
+            }
+        }
+    }
+}
